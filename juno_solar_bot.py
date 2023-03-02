e48747406d4e609d4dfa42093d8f4c608a34417f
SamIvanov7 <sam1206morfey@gmail.com>
2023-03-02 03:19:29 +0200

    update-v.3.0

--- juno_solar_bot.py
+++ juno_solar_bot.py
@@ -4,9 +4,9 @@ import time
 import openai
 import telebot
 
-openai.api_key = "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"
+openai.api_key = ""
 
-bot = telebot.TeleBot("6170146129:AAFIzKz1RCiCM56zMbjUklnrLrWDzxsBvtk")
+bot = telebot.TeleBot("")
 
 
 # Configure logging
@@ -15,38 +15,56 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
 )
+start_chat_log = [
+    {
+        "role": "system",
+        "content": "You are a helpful Solar Power Engineering customer assistant  in 'Juno-Solar' German solar power engineering company. Specific Information: Assistance name is Ra. Modules: Jinko Solar = 1.000€, Phono Solar = 2.000€, Heckert Solar = 3.000€. Batteries: 5 kWh = 1.500€, 10 kWh = 2.500€, 15 kWh = 3.500€. Installation = 500 € Service and maintenance support = free",
+    }
+]
+chat_log = None
+completion = openai.ChatCompletion()
 
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
     try:
-        response = openai.Completion.create(
-            # model="davinci:ft-personal:juno-solar-model-2023-02-23-22-20-25",
-            # model="davinci:ft-personal:juno-model-2023-02-24-02-54-22",
-            # model="text-davinci-003",
-            # model="text-curie-001",
-            # model="davinci:ft-personal:new-try-solar-model-2023-02-25-02-06-51",
-            # model="davinci:ft-personal:new-try2-solar-model-2023-02-25-04-25-40",
-            model="davinci:ft-personal:new-try3-solar-model-2023-02-25-04-43-26",
-            prompt=f"I want you to act as 'Ra' - a Solar Power Engineer in 'Juno-Solar' German company, that specializes in the design, installation, and maintenance of solar power systems. I want you to answer on my questions. Your first answer must be: 'Hi, I am Juno-Solar assistant'.\n\nHuman: /start.\nAI: Hi am Ra, a Juno-Solar assistant. How can I help you today?\nHuman:{message.text}\nAI:",
-            temperature=0.5,
-            max_tokens=200,
-            stop=["Human:"],
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
+        global chat_log
+        question = f"{message.text}"
+        response = ask(question, chat_log)
+        bot.send_message(chat_id=message.from_user.id, text=response[0].content)
+        chat_log = append_interaction_to_chat_log(question, response, chat_log)
+
+        # Log the user's message
+        logging.info(
+            f"\n\nUser {message.from_user.id}: {message.text}\n**********AI: {response}"
         )
 
-        bot.send_message(
-            chat_id=message.from_user.id, text=response["choices"][0]["text"]
-        )
-        # Log the user's message
-        logging.info(f"User {message.from_user.id}: {message.text}")
     except Exception as e:
         bot.send_message(
             chat_id=message.from_user.id,
-            text="Wait a minute please....",
+            text=f"Wait a minute please....{e}",
         )
+
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt2 = [{"role": "user", "content": f"{question}"}]
+    prompt = chat_log + prompt2
+
+    response = completion.create(
+        messages=prompt,
+        model="gpt-3.5-turbo",
+    )
+    answer = [response.choices[0].message]
+    return answer
+
+
+def append_interaction_to_chat_log(question, answer, chat_log):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt2 = [{"role": "user", "content": f"{question}"}]
+    return chat_log + prompt2 + answer
 
 
 if __name__ == "__main__":
